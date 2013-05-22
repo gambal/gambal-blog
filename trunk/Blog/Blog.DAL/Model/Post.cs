1bfc2464e2104dbfa82f0eb4ccd85749eea9e0c1
gambal <gambalek@gmail.com>
2013-05-22 19:01:23 +0200

    commit

---
+++ trunk/Blog/Blog.DAL/Model/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Blog.DAL.Model
+{
+    public class Post
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
